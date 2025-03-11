bd21eccb99f72b1a7e1f4195e16b01f5448a46db
rjsgml04 <dbsrjsgml04@gmail.com>
2025-03-11 20:53:53 +0900

    변경

--- src/class1/ClassStart2.java
+++ src/class1/ClassStart2.java
@@ -1,21 +1,18 @@
 package class1;
 
 /**
- *  학생이 늘어날때마다 변수를 추가로 선언해야하는 문제 발생
- *  -> 일단은 배열을 통해 문제를 해결해보는 과정으로 진행
+ * 배열로 수정
+ *  예를들어 데이터 제거시에는 정확한 인덱스에 내용을 제거해야 하기에 이또한 좋은 방법은 아님
  */
 public class ClassStart2 {
     public static void main(String[] args) {
-        String student1Name = "학생1";
-        int student1Age = 15;
-        int student1Grade = 90;
+        String[] studentNames = {"학생1","학생2"};
+        int[] studentAges = {15,16};
+        int[] studentGrade = {90,80};
 
-        String student2Name = "학생2";
-        int student2Age = 16;
-        int student2Grade = 80;
-
-        System.out.println("이름:" + student1Name + "나이:" + student1Age + "성적:" + student1Grade);
-        System.out.println("이름:" + student2Name + "나이:" + student2Age + "성적:" + student2Grade);
+        for(int i=0;i<studentNames.length;i++){
+            System.out.println("이름:" + studentNames[i] + "나이:" + studentAges[i] + "성적:" + studentGrade[i]);
+        }
 
     }
 }
